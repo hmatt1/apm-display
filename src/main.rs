586f2e88494addee554cca0cbcb92c938e5b877f
hmatt1 <hornung.m1@gmail.com>
2020-12-22 16:55:24 -0600

    Clock increments counter every 500ms

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,48 @@
-use iced::{button, Align, Button, Column, Element, Sandbox, Settings, Text};
+use iced::{
+    canvas::{self, Cache, Canvas, Cursor, Geometry, LineCap, Path, Stroke},
+    executor, time, Application, Color, Command, Container, Element, Length,
+    Point, Rectangle, Settings, Subscription, Vector, Button, button, Column, Align, Text
+};
 
 pub fn main() -> iced::Result {
     Counter::run(Settings::default())
 }
 
-#[derive(Default)]
 struct Counter {
     value: i32,
     increment_button: button::State,
     decrement_button: button::State,
+    now: chrono::DateTime<chrono::Local>,
+    clock: Cache,
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
     IncrementPressed,
     DecrementPressed,
+    Tick(chrono::DateTime<chrono::Local>),
 }
 
-impl Sandbox for Counter {
+impl Application for Counter {
+    type Executor = executor::Default;
     type Message = Message;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self::default()
+    fn new(_flags: ()) -> (Self, Command<Message>) {
+        (Counter {
+            value: 0,
+            increment_button: Default::default(),
+            decrement_button: Default::default(),
+            now: chrono::Local::now(),
+            clock: Default::default()
+        }, Command::none())
     }
 
     fn title(&self) -> String {
         String::from("Counter - Iced")
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::IncrementPressed => {
                 self.value += 1;
@@ -36,13 +50,26 @@ impl Sandbox for Counter {
             Message::DecrementPressed => {
                 self.value -= 1;
             }
+
+            Message::Tick(local_time) => {
+                let now = local_time;
+
+                if now != self.now {
+                    self.now = now;
+                    self.clock.clear();
+                }
+                self.value += 1;
+            }
         }
+
+        Command::none()
     }
 
     fn view(&mut self) -> Element<Message> {
         Column::new()
             .padding(20)
             .align_items(Align::Center)
+            .width(Length::Fill)
             .push(
                 Button::new(&mut self.increment_button, Text::new("Increment"))
                     .on_press(Message::IncrementPressed),
@@ -52,6 +79,12 @@ impl Sandbox for Counter {
                 Button::new(&mut self.decrement_button, Text::new("Decrement"))
                     .on_press(Message::DecrementPressed),
             )
+            .push(Text::new(self.now.to_string()).size(50))
             .into()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        time::every(std::time::Duration::from_millis(500))
+            .map(|_| Message::Tick(chrono::Local::now()))
     }
 }
